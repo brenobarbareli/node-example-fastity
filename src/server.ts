72171fdef806ea3c25165d5a47436185cb230065
brenobarbareli <breno_barbareli@hotmail.com>
2024-04-04 12:05:51 -0300

    feat: new rout to get attendee badge

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import {
   validatorCompiler,
 } from 'fastify-type-provider-zod'
 import { createEvent } from './routes/create-event'
+import { getAttendeeBadge } from './routes/get-attendee-badge'
 import { getEvent } from './routes/get-event'
 import { registerForEvent } from './routes/register-for-event'
 
@@ -15,6 +16,7 @@ app.setSerializerCompiler(serializerCompiler)
 app.register(createEvent)
 app.register(registerForEvent)
 app.register(getEvent)
+app.register(getAttendeeBadge)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('server is running')
