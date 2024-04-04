a44bb00557ff6260d7458a044417bd99f00976b9
brenobarbareli <breno_barbareli@hotmail.com>
2024-04-04 11:43:20 -0300

    feat: new route for get event

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import {
   validatorCompiler,
 } from 'fastify-type-provider-zod'
 import { createEvent } from './routes/create-event'
+import { getEvent } from './routes/get-event'
 import { registerForEvent } from './routes/register-for-event'
 
 const app = fastify()
@@ -13,6 +14,7 @@ app.setSerializerCompiler(serializerCompiler)
 
 app.register(createEvent)
 app.register(registerForEvent)
+app.register(getEvent)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('server is running')
