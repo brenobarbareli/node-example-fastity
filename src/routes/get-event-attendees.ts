47c944ab802bf0d0b65ab22c9977db4142f5792a
brenobarbareli <breno_barbareli@hotmail.com>
2024-04-05 12:05:26 -0300

    finishing swagger

--- src/routes/get-event-attendees.ts
+++ src/routes/get-event-attendees.ts
@@ -8,6 +8,8 @@ export async function getEventAttendees(app: FastifyInstance) {
     '/events/:eventId/attendees',
     {
       schema: {
+        summary: 'get event attendees',
+        tags: ['attendees'],
         params: z.object({
           eventId: z.string().uuid(),
         }),
